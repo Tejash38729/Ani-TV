6aafcb49661dd30756daf586df3bc0ba98fe75fb
Pythonwithsean <Lunafcs@Protonmail.com>
2024-02-02 09:50:44 +0000

    "Anime Grid"

--- Client/src/components/Home.tsx
+++ Client/src/components/Home.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import { useGlobalcontext } from "../contexts/GlobalProvider";
+import CircularProgress from "@mui/material/CircularProgress";
 
 //######################################################################################################################
 
@@ -15,80 +16,95 @@ type elementData = {
   };
 };
 
+type elementImage = {
+  images: {
+    jpg: {
+      image_url: string;
+    };
+  };
+};
+
 //################################################################################################################
 
 //Home React Func
 const Home = () => {
   //Render LOGIC
   const { animeWallpaper } = useGlobalcontext() as any;
+  const { popularAnime } = useGlobalcontext() as any;
+
   useEffect(() => {
     setData(animeWallpaper);
-  }, [animeWallpaper]);
-
-  if (animeWallpaper) {
-    console.log(animeWallpaper);
-  }
-
-  console.log(animeWallpaper);
+    setImage(
+      popularAnime.map((element: elementImage) => element.images.jpg.image_url)
+    );
+    setLoaded(true);
+  }, [animeWallpaper, popularAnime]);
 
   //Set Quote Render Quote and ask user if they know the character that made this quote
   const [image, setImage] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   const [data, setData] = useState([]);
   const [quote, setQuote] = useState();
-  // const { animeWallpaper } = useGlobalcontext();
-  // setImage(animeWallpaper);
+
   //###############################################################################################################################################################
   //Return
 
   return (
     <div>
-      <Carousel
-        className="Carousel"
-        swipe={true}
-        stopAutoPlayOnHover={false}
-        autoPlay={true}
-        indicators={false}
-        animation="fade"
-        cycleNavigation={true}
-        duration={300}
-      >
-        {data.length !== 0
-          ? data.map((e: elementData, i) => {
-              return (
-                <img
-                  key={i}
-                  src={`${e.attributes.coverImage.original}`}
-                  width={3360}
-                  height={800}
-                  alt=""
-                />
-              );
-            })
-          : null}
-      </Carousel>
-      <Grid
-        container
-        // id="Container"
-        spacing={2}
-        direction="row"
-        justifyContent="flex-start"
-      >
-        <Grid item xs={6}>
-          {" "}
-          {image
-            ? image.map((e, i) => {
+      {loaded ? (
+        <Carousel
+          className="Carousel"
+          swipe={true}
+          stopAutoPlayOnHover={false}
+          autoPlay={true}
+          indicators={false}
+          animation="fade"
+          cycleNavigation={true}
+          duration={300}
+        >
+          {data.length !== 0 && loaded === true
+            ? data.map((e: elementData, i) => {
                 return (
                   <img
-                    className="AnimeImages"
                     key={i}
-                    src={`${e}`}
-                    alt="Anime"
+                    src={`${e.attributes.coverImage.original}`}
+                    width={3360}
+                    height={800}
+                    alt=""
                   />
                 );
               })
             : null}
+        </Carousel>
+      ) : (
+        <CircularProgress className="loader" color="secondary" />
+      )}
+      <section className="Container">
+        <h1>Popular Anime</h1>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          marginTop={5}
+          justifyContent="center"
+        >
+          {" "}
+          {image
+            ? image.map((e, i) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={i}>
+                    <img
+                      className="AnimeImages"
+                      key={i}
+                      src={`${e}`}
+                      alt={`Item ${i}`}
+                    />
+                  </Grid>
+                );
+              })
+            : null}
         </Grid>
-      </Grid>
+      </section>
     </div>
   );
 };
